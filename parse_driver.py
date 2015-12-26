edb2a4cd8dd5c80543795efbe5ac394002925513
Kelvin Leong <kelvin.leong@berkeley.edu>
2015-12-25 23:55:19 -0800

    go service retrieves url, but does not redirect

---
+++ parse_driver.py
@@ -0,0 +1,64 @@
+import config
+import json, httplib, urllib
+
+def get_connection():
+	con = httplib.HTTPSConnection('api.parse.com', 443)
+	con.connect()
+	return con
+
+def make_parse_get_request(url, params={}):
+	connection = get_connection()
+	params = urllib.urlencode(params)
+	connection.request(
+		'GET',
+		url + '?%s' % params,
+		'',
+		{
+			"X-Parse-Application-Id": config.PARSE_APP_ID,
+			"X-Parse-REST-API-Key": config.PARSE_REST_API_KEY
+		}
+	)
+	result = json.loads(connection.getresponse().read())
+	return result
+
+def make_parse_post_request(url, obj):
+	connection = get_connection()
+	connection.request(
+		'POST',
+		url,
+		json.dumps(obj),
+		{
+			"X-Parse-Application-Id": config.PARSE_APP_ID,
+			"X-Parse-REST-API-Key": config.PARSE_REST_API_KEY
+		}
+	)
+	result = json.loads(connection.getresponse().read())
+	return result
+
+def make_parse_put_request(url, obj):
+	connection = get_connection()
+	connection.request(
+		'PUT',
+		url,
+		json.dumps(obj),
+		{
+			"X-Parse-Application-Id": config.PARSE_APP_ID,
+			"X-Parse-REST-API-Key": config.PARSE_REST_API_KEY
+		}
+	)
+	result = json.loads(connection.getresponse().read())
+	return result
+
+def make_parse_delete_request(url, obj):
+	connection = get_connection()
+	connection.request(
+		'DELETE',
+		url,
+		'',
+		{
+			"X-Parse-Application-Id": config.PARSE_APP_ID,
+			"X-Parse-REST-API-Key": config.PARSE_REST_API_KEY
+		}
+	)
+	result = json.loads(connection.getresponse().read())
+	return result
---
+++ run.py
@@ -0,0 +1,49 @@
+import config
+
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session, Response, make_response
+from flask.ext.cors import CORS
+import sys, json, datetime
+
+# Kelvin
+import parse_driver as ParseDriver
+
+app = Flask(__name__)
+CORS(app)
+
+# *********************** ROUTES ***********************
+
+@app.route('/')
+def hello():
+	return 'Kelvin Leong website'
+
+@app.route('/test')
+def test_route():
+	return 'testing'
+
+# *********************** GO LINKS ***********************
+
+@app.route('/go/<key>')
+def go(key):
+	params = {'where': json.dumps({'key': key})}
+	golinks = ParseDriver.make_parse_get_request('/1/classes/ParseGoLink', params)['results']
+	if len(golinks) == 0:
+		return 'not a valid key'
+	else:
+		return redirect(golinks[0]['url'])
+
+@app.route('/create_golink', methods=['POST'])
+def create_golink():
+	# form = request.get_json()
+	# golink = {	'key': form.get('key'),
+				# 'url': form.get('url') }
+
+	golink = {	'key': 'asdasd',
+				'url': 'asdasdasd' }
+
+	ParseDriver.make_parse_post_request('/1/classes/ParseGoLink', golink)
+	return 'ok'
+
+# *********************** LOGIC ***********************
+
+if __name__ == '__main__':
+	app.run()

56d9e3aa12fe836ebe43e66a579fe278467b261a
Kelvin Leong <kelvin.leong@berkeley.edu>
2015-12-29 13:22:07 -0800

    lookup route works

--- golinks/routes.py
+++ golinks/routes.py
@@ -29,33 +29,44 @@ def test_route():
 
 @app.route('/go/<key>')
 def go(key):
-	# decode from encoded version on Parse
-	decoded_key = urllib2.quote(key, 'utf-8')
+	params = {'where': json.dumps({'key': key})}
 
-	params = {'where': json.dumps({'key': decoded_key})}
+	# Parse GET request will already encode the params, so no need to encode twice
 	golinks = ParseDriver.make_parse_get_request('/1/classes/ParseGoLink', params)['results']
 	
 	if len(golinks) == 0:
-		return 'not a valid key'
+		return 'Not a valid key'
 	else:
-		# decode from encoded version on Parse
+		# decode from encoded version from Parse
 		decoded_url = urllib2.unquote(golinks[0]['url'])
 		return redirect(decoded_url)
 
 @app.route('/create_golink', methods=['POST'])
 def create_golink():
+	# POST request will not encode, so ensure these are already encoded
+
 	golink = {	'key': request.form['key'],
 				'url': request.form['url'] }
 
 	ParseDriver.make_parse_post_request('/1/classes/ParseGoLink', golink)
 	return 'ok'
 
-@app.route('/lookup', methods=['GET'])
+@app.route('/lookup')
 def lookup():
-	params = {'where': json.dumps({'url': request.form['url']})}
+	try:
+		# URL should already be encoded
+		url = request.args.get('url', '')
+	except KeyError:
+		# may want to return an empty JSON instead
+		return 'No key found in the lookup function'
 
+	params = {'where': json.dumps({'url': url})}
 	golinks = ParseDriver.make_parse_get_request('/1/classes/ParseGoLink', params)['results']
-	if len(golinks) == 0:
-		return 'No previous go-links found'
-	else:
-		return golinks[0]['keys']
+
+	matching_golinks = []
+	# check for multiple go-links that map to same URL
+	for g in golinks:
+		decoded_key = urllib2.unquote(g['key'])
+		matching_golinks.append(decoded_key)
+
+	return json.dumps(matching_golinks)
